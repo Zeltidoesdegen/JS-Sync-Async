015d8fb1dc41cf5e39c30432ed0aa07ccb01f190
Zeltidoesdegen <ivan.carsula@gmail.com>
2023-08-15 13:41:47 +0800

    JS notes and updates

--- 01Asynchronous/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ 01Asynchronous/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -9,24 +9,51 @@
 
 // WORKING!!!!
 
-const jokeDiv = document.querySelector("#joke");
-const getjokeBtn = document.querySelector("#joke-btn");
+// ###########################################################
+// My answer
+// const jokeDiv = document.querySelector("#joke");
+// const getjokeBtn = document.querySelector("#joke-btn");
 
-function fetchRndmJk() {
-    const jokes = new XMLHttpRequest();
-    jokes.open("GET", "https://api.chucknorris.io/jokes/random");
+// function fetchRndmJk() {
+//     const jokes = new XMLHttpRequest();
+//     jokes.open("GET", "https://api.chucknorris.io/jokes/random");
 
-    jokes.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            const jkdata = JSON.parse(this.responseText);
-            jokeDiv.innerHTML = jkdata.value;
+//     jokes.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//             const jkdata = JSON.parse(this.responseText);
+//             jokeDiv.innerHTML = jkdata.value;
+//         }
+//     };
+//     jokes.send();
+// }
+
+// getjokeBtn.addEventListener("click", function (){
+//     fetchRndmJk();
+// });
+
+// fetchRndmJk();
+
+// ###########################################################
+// Efficient way, using DOMcontentloaded
+
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById("joke-btn")
+
+const generateJoke = () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api.chucknorris.io/jokes/random")
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                jokeEl.innerHTML = JSON.parse(this.responseText).value;
+            } else {
+                jokeEl.innerHTML = "Something Went Wrong (It's not funny!!!)";
+            }
         }
     };
-    jokes.send();
-}
-
-getjokeBtn.addEventListener("click", function (){
-    fetchRndmJk();
-});
-
-fetchRndmJk();
+    xhr.send();
+};
+jokeBtn.addEventListener("click", generateJoke);
+document.addEventListener("DOMContentLoaded",generateJoke);
--- 01Asynchronous/06-callback-hell/script.js
+++ 01Asynchronous/06-callback-hell/script.js
@@ -1,54 +1,55 @@
 //Introduction
-//this will generate the movies, actors, directors in no particular order because of the random setTimeout
+//this will generate the movies, actors, directors in no particular order because of the random setTimeout (1-4 seconds)
 //we do not have control over the output, thus we have to use nested callbacks (callback hell)
-// //OUTPUT IS IN NO PARTICULAR ORDER
+
+
+//##########################################
+//Output is in no particular order
 // function getData(endpoint) {
 //     const xhr = new XMLHttpRequest();
 
 //     xhr.open("GET", endpoint);
 
 //     xhr.onreadystatechange = function () {
-//         if (this.readyState === 4 && this.status === 200) {
-//             console.log(JSON.parse(this.responseText));
+//         if (this.readyState === 4 && this.status === 200){
+//          console.log(JSON.parse(this.responseText));
 //         }
-//     };
-
-//     setTimeout(() => {
-//         xhr.send();
-//     },Math.floor(Math.random() * 3000) + 1000);
-
-// }
-
-// getData("./movies.json")
-// getData("./actors.json")
-// getData("./directors.json")
-
-//OUTPUT IS IN NO PARTICULAR ORDER
+//      };
+     
+//      //random 1-5 seconds
+//      setTimeout(() => {
+//          xhr.send();        
+//      },Math.floor(Math.random() * 4000) + 1000);
+//     }
+    
+//     getData("./movies.json")
+//     getData("./actors.json")
+//     getData("./directors.json")
+//##########################################
+//controlling the output
 function getData(endpoint,cb) {
     const xhr = new XMLHttpRequest();
 
     xhr.open("GET", endpoint);
 
     xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            cb(JSON.parse(this.responseText));
+        if (this.readyState === 4 && this.status === 200){
+         cb(JSON.parse(this.responseText));
         }
-    };
+     };
 
-    setTimeout(() => {
-        xhr.send();
-    },Math.floor(Math.random() * 3000) + 1000);
-
+     //random 1-5 seconds
+     setTimeout(() => {
+        xhr.send();        
+     },Math.floor(Math.random() * 3000) + 1000);
 }
 
 //Movies muna
 // getData("./movies.json", (data) => {
 //     console.log(data);
 // })
-// getData("./actors.json")
-// getData("./directors.json")
 
-//movies then actors
+//Movies then actors
 // getData("./movies.json", (data) => {
 //     console.log(data);
 //     getData("./actors.json", (data) => {
@@ -56,7 +57,7 @@ function getData(endpoint,cb) {
 //     })
 // })
 
-//movies then actors then directors
+//Movies, then actors, then directors
 getData("./movies.json", (data) => {
     console.log(data);
     getData("./actors.json", (data) => {
